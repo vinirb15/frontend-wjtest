0533f4e644ec0b2c878672a99b298b96247a7405
vbueno <viniciusbueno15@gmail.com>
2021-06-11 18:19:57 -0300

    [build]: config

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,9 @@ import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import Home from './pages/home';
+import Sapatos from './pages/sapatos';
+import Camisetas from './pages/camisetas';
+import Calca from './pages/calcas';
 import Header from './components/header';
 import Footer from './components/footer';
 
@@ -10,6 +13,9 @@ const Routes = () => (
         <Header />
         <Switch>
             <Route exact path="/" component={Home} />
+            <Route path="/sapatos" component={Sapatos} />
+            <Route path="/camisetas" component={Camisetas} />
+            <Route path="/calcas" component={Calca} />
         </Switch>
         <Footer />
     </BrowserRouter>
